8dc6ed350dcd2417f5d6c8bfd7aaacdf2f63400f
1262695 <1262695@csfoy.ca>
2022-12-07 10:10:01 -0500

    router avancer manque tester pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,31 @@
+import { Container } from 'react-bootstrap';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BarreNavigation } from './BarreNavigation';
+import { pageAccueil } from './pages/pageAccueil';
+import { pageAdministration } from './pages/pageAdministration';
+import { pageAjouter } from './pages/pageAjouter';
+import { pageModifier } from './pages/pageModifier';
+import { pageRepertoire } from './pages/pageRepertoire';
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Container>
+        <BarreNavigation />
+        <Routes>
+          <Route path='/' element={<pageAcceuil />}/>
+          <Route path='/admin' element={<pageAdministration />}/>
+          <Route path='/ajout' element={<pageAjouter />}/>
+          <Route path='/modifier' element={<pageModifier />}/>
+          <Route path='/repertoire' element={<pageRepertoire />}/>
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/BarreNavigation.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+
+
+export const BarreNavigation = () => {
+    return(
+    <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Repertoire Musicale</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Accueil</Nav.Link>
+            <Nav.Link href="/ajout">Ajout</Nav.Link>
+            <Nav.Link href="/modifier">Modifier</Nav.Link>
+            <Nav.Link href="/repertoire">Repertoire</Nav.Link>
+            
+          </Nav>
+          <Nav>
+            <Nav.Link href="/admin">Admin</Nav.Link>
+          </Nav>
+        </Container>
+    </Navbar>
+    )
+}
