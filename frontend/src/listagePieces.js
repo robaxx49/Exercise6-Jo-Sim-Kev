fc423002fa700dfde6e6730f397da803266c841d
Simon Renaud <simon_renaud@live.fr>
2022-12-07 10:18:40 -0500

    Ébauche de tableau d'affichage des pièces

---
+++ frontend/src/listagePieces.js
@@ -0,0 +1,41 @@
+import React from "react";
+import {Row, Col, Button} from "react-bootstrap";
+
+export function listagePieces({listePieces, isAdmin})
+{
+    let boutonAdmin = null;
+
+    if (isAdmin)
+    {
+        boutonAdmin = <Bouton>Modifier</Bouton>;
+    }
+
+    return(
+        <Row>
+            <Col>
+                <Table bordered striped>
+                    <thead>
+                        <tr>
+                            <th>Titre</th>
+                            <th>Artiste</th>
+                            <th>Catégorie</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {listePieces.map(u =>
+                            <tr>
+                                <td>{u.titre}</td>
+                                <td>{u.artiste}</td>
+                                <td>{u.categorie}</td>
+                                <td>{boutonAdmin}</td>
+                            </tr>
+                        )};
+                    </tbody>
+                </Table>
+            </Col>
+        </Row>
+    )
+}
+
+export default listagePieces;
