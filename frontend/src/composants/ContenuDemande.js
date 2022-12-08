373a5724f9b264c1674c54e77d24f6c1b3cb8bde
Simon Renaud <simon_renaud@live.fr>
2022-12-08 14:57:46 -0500

    Structure de base des nouvelles pages et composantes

---
+++ frontend/src/composants/ContenuDemande.js
@@ -0,0 +1,8 @@
+import { React, useState } from 'react';
+
+export function ContenuDemande()
+{
+    ;
+}
+
+export default ContenuDemande;
---
+++ frontend/src/pages/PageAfficherDemande.js
@@ -0,0 +1,25 @@
+import { React, useState } from 'react';
+import ListeDemandes from '../composants/ListeDemandes';
+
+export function PageAfficherDemande()
+{
+    const [listeDemandes, setListeDemandes] = useState([]);
+
+    useEffect(() => {
+        const chercherDonnees = async () => {
+            const resultat = await fetch(`/api/demandes`);
+            const body = await resultat.json().catch((error) => {console.log(error)});
+            setListeDemandes(body);
+        };
+        chercherDonnees();
+    }, []);
+
+    return (
+        <>
+            <h1>Liste des demandes spéciales</h1>
+            <ListeDemandes demandes={listeDemandes} />
+        </>
+    )
+}
+
+export default PageAfficherDemande;
