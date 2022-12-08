a0bc8eae148f842997281301333739da7f861112
1262695 <1262695@csfoy.ca>
2022-12-08 14:29:30 -0500

    2 nouveau get exercise 2

---
+++ backend/src/bd/getDemande.js
@@ -0,0 +1,14 @@
+import { utiliserDB } from './connection';
+
+export const getDemande = (requete, reponse) => {
+    
+
+    utiliserDB(async (db) => {
+        
+        const infoDemandes = await db.collection('demande-special').find().ToArray();
+        
+        reponse.status(200).json(infoDemandes);      
+    }, reponse).catch(
+        () => reponse.status(500).send("Aucune demande special en memoire")
+    );
+};
---
+++ backend/src/bd/getDemandeParID.js
@@ -0,0 +1,17 @@
+import { ObjectId } from 'mongodb';
+
+import { utiliserDB } from './connection';
+
+export const getPiece = (requete, reponse) => {
+    const id = requete.params.id;
+
+    utiliserDB(async (db) => {
+        var objectId = ObjectId.createFromHexString(id);
+
+        const infoDemande = await db.collection('demande-special').findOne({ _id: objectId });
+        
+        reponse.status(200).json(infoDemande);      
+    }, reponse).catch(
+        () => reponse.status(500).send("Demande non trouvée")
+    );
+};
